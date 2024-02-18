be9f6e89161358e488163287be9a113b08638413
Thoyaz <64622635+Thoyaz@users.noreply.github.com>
2024-02-19 01:37:28 +0530

    Update jfrogAdd.groovy

--- vars/jfrogAdd.groovy
+++ vars/jfrogAdd.groovy
@@ -2,4 +2,5 @@ def call(){
     sh 'ls'
     sh 'cd target/'
     sh 'curl -X PUT -u admin:Thug@12345 -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://20.235.242.133:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+    sh 'ls'
 }
